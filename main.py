6e2c84efb6e347473872a45e035cba715961eaee
Janko Prester <janko.prester@corsmed.com>
2025-01-05 15:01:52 +0100

    initial commit

---
+++ main.py
@@ -0,0 +1,25 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+app = FastAPI()
+
+# Add CORS middleware
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Allows all origins
+    allow_credentials=True,
+    allow_methods=["*"],  # Allows all methods
+    allow_headers=["*"],  # Allows all headers
+)
+
+@app.get("/")
+async def read_root():
+    return {"message": "Hello, FastAPI 222!"}
+
+@app.get("/test")
+async def read_test():
+    return {"message": "Hello, Test!"}
+
+@app.get("/items/{item_id}")
+async def read_item(item_id: int, q: str = None):
+    return {"item_id": item_id, "q": q}
