4c5ce2df0e5d2bdcbacf4cd22eba36f579e99847
Janko Prester <janko.prester@corsmed.com>
2025-01-06 14:10:57 +0100

    implement mistral completion endpoint

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
+import os
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+from mistralai import Mistral
+from dotenv import load_dotenv
+from pydantic import BaseModel
 
 app = FastAPI()
 
@@ -12,13 +16,37 @@ app.add_middleware(
     allow_headers=["*"],  # Allows all headers
 )
 
-@app.get("/")
-async def read_root():
-    return {"message": "Hello, FastAPI 222!"}
+# Load environment variables from the .env file
+load_dotenv()
 
-@app.get("/test")
-async def read_test():
-    return {"message": "Hello, Test!"}
+api_key = os.getenv("MISTRAL_API_KEY")
+
+@app.get("/")
+async def check():
+    return {"message": "API is working!"}
+
+class CompletionRequest(BaseModel):
+    content: list
+    model: str = None
+
+@app.post("/completion")
+async def get_completion(request: CompletionRequest):
+    if not request.content:
+        return {"error": "Content cannot be empty"}
+    if not api_key:
+        return {"error": "API key not found"}
+
+    model = request.model if request.model else "mistral-tiny"
+    client = Mistral(api_key=api_key)
+
+    try:
+        chat_response = client.chat.complete(
+            model=model,
+            messages=request.content,
+        )
+        return {"response": chat_response.choices[0].message.content}
+    except Exception as e:
+        return {"error": str(e)}
 
 @app.get("/items/{item_id}")
 async def read_item(item_id: int, q: str = None):
